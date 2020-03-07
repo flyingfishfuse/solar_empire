43f09077042bbf7fa355ae55a39773e2072142cd
flyingfishfuse <flyingfishfuse@github.com>
2020-03-07 13:38:44 -0800

    modified:   solar_empire/models.py

--- solar_empire/models.py
+++ solar_empire/models.py
@@ -1,25 +1,37 @@
 import solar_empire
-from flask_sqlalchemy import SQLAlchemy
+from solar_empire import database
 
 class User(database.Model):
-    login_id      = database.Column(database.Integer, primary_key=True)
-    username      = database.Column(database.String(64), index=True, unique=True)
-    email         = database.Column(database.String(120), index=True, unique=True)
-    password_hash = database.Column(database.String(128))
-    ship_id       = database.Column(database.String(128))
-    location      = database.Column(database.String(128))
-    turns         = database.Column(database.Integer)
-    turns_run     = database.Column(database.Integer)
-    cash          = database.Column(database.Integer)
+    login_id        = database.Column(database.Integer, primary_key=True)
+    username        = database.Column(database.String(64), index=True, unique=True)
+    email           = database.Column(database.String(120), index=True, unique=True)
+    password_hash   = database.Column(database.String(128))
+    ship_id         = database.Column(database.String(128))
+    location        = database.Column(database.String(128))
+    turns           = database.Column(database.Integer)
+    turns_run       = database.Column(database.Integer)
+    safe_turns_left = database.Column(database.Integer)
+    cash            = database.Column(database.Integer)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
 class UserShip(database.Model):
-	ship_id          = database.Column(database.String(128))
-	location         = database.Column(database.String(128))
-	configuration    = database.Column(database.String(128))
-    #quipment        = database.Column(database.String(128))
+    ship_id          = database.Column(database.String(128))
+    location         = database.Column(database.String(128))
+    configuration    = database.Column(database.String(128))
+    equipment        = database.Column(database.String(128))
+
+class PublicPost(database.Model):
+    time = database.Column(database.String(128))
+    content = database.Column(database.String(528))
+    user_id = database.Column(database.String(128))
+
+class ClanPost(database.Model):
+    clan = database.Column(database.String(128))
+    time = database.Column(database.String(128))
+    content = database.Column(database.String(528))
+    user_id = database.Column(database.String(128))
 
 class GameVars(database.Model):
     score_method             = database.Column(database.Integer)
@@ -29,7 +41,7 @@ class GameVars(database.Model):
     logged_out_players       = database.Column(database.Integer)
     ships_destroyed          = database.Column(database.Integer)
     clans                    = database.Column(database.Integer)
-    safe_turns_left          = database.Column(database.Integer)
+    global_safe_turns_left   = database.Column(database.Integer)
     newbies_left             = database.Column(database.Integer)
     sudden_death             = database.Column(database.Boolean)
     sudden_death_turns_feft  = database.Column(database.Integer)
