438c328200b532af52255464f6b5ef1397e2dc4c
flyingfishfuse <flyingfishfuse@github.com>
2020-03-08 04:53:13 -0700

    On branch master  Changes to be committed: 	new file:   solar_empire/attack.py 	new file:   solar_empire/configuration_options.py 	new file:   solar_empire/inc/generator_functions.py 	new file:   solar_empire/templates/diary.html 	new file:   solar_empire/templates/gamevars.html

---
+++ solar_empire/configuration_options.py
@@ -0,0 +1,39 @@
+import os
+from flask.config import Config
+import solar_empire
+from solar_empire.models import GameVars
+
+VERSION            = 'Generic SE 2.9.1 - MODIFIED:Python Language Conversion -  Version : 0.01'
+DATABASE_HOST      = "localhost"
+DATABASE           = "solar_empire-python"
+DATABASE_USER      = "moop"
+DATABASE_PASSWORD  = "password"
+
+SERVER_NAME        = "Solar Empire: 2020 - Python Edition"
+SERVER_ADDRESS     = ('localhost', 4443)
+HTTP_HOST          = "gamebiscuits"
+SERVER_HOST        = "fightbiscuits"
+DOMAIN             = "firewall-gateway.net"
+GAME_DIR           = "/solar_empire/"
+URL_FULL           = "https://" + HTTP_HOST + SERVER_HOST + DOMAIN + GAME_DIR
+URL_SHORT          = "asdf"
+SEND_AUTH_MAIL     = True
+SESSION_TIME_LIMIT = 3600
+
+ADMIN_NAME = "Emperor of Sol"
+ADMIN_PASSWORD = "password"
+ADMIN_EMAIL = "game_admin" + "@" + HTTP_HOST + SERVER_HOST + DOMAIN
+ADMIN_USER_ID = 1
+OWNER_ID = 1
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+things_this_app_needs = ['flask' , "flask-sqlalchemy"]
+
+class Config(object):
+    # ...
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE + '.' + HTTP_HOST + '.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+GameVars.sv_turns = 30
+QUARK_DAMAGE = random.randint(400,1000)
